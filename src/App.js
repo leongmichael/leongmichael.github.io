c998b04ecebff844d1ca8d7d5f4c55451eaa95e6
leongmichael <michaelleong1229@gmail.com>
2025-03-06 02:18:35 -0800

    delete unused

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import { useContext } from 'react'
 import { ThemeContext } from './contexts/theme'
-import Header from './components/Header/Header'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
-import Skills from './components/Skills/Skills'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
-import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
-import Publications from './components/Publications/Publications'
 import PersonalWebsite from './components/PersonalWebsite'
 import './App.css'
 
